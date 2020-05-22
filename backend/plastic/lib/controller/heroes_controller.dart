a91ca704b30185ce5720cb2b2bbdbcad91b98cfb
Matthew Popp <mgp214@gmail.com>
2020-05-21 20:35:11 -0700

    initialized frontend and backend apps
    closes #1, closes #2

---
+++ backend/plastic/lib/controller/heroes_controller.dart
@@ -0,0 +1,29 @@
+import 'package:aqueduct/aqueduct.dart';
+import 'package:plastic/plastic.dart';
+
+class HeroesController extends ResourceController {
+  final _heroes = [
+    {'id': 11, 'name': 'Mr. Nice'},
+    {'id': 12, 'name': 'Narco'},
+    {'id': 13, 'name': 'Bombasto'},
+    {'id': 14, 'name': 'Celeritas'},
+    {'id': 15, 'name': 'Magneta'},
+  ];
+
+  @Operation.get()
+  Future<Response> getAllHeroes() async {
+    return Response.ok(_heroes);
+  }
+
+  @Operation.get('id')
+  Future<Response> getHeroByID(@Bind.path('id') int id) async {
+    final hero =
+        _heroes.firstWhere((hero) => hero['id'] == id, orElse: () => null);
+
+    if (hero == null) {
+      return Response.notFound();
+    }
+
+    return Response.ok(hero);
+  }
+}
---
+++ backend/plastic/lib/plastic.dart
@@ -0,0 +1,11 @@
+/// plastic
+///
+/// A Aqueduct web server.
+library plastic;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
