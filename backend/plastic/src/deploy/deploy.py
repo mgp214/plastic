77e6230d02ce4becadc79dd8be96c0b10f1eec10
Matthew Popp <mgp214@gmail.com>
2020-05-24 01:52:25 -0700

    simple deployment script
    closes #18

---
+++ backend/plastic/src/deploy/deploy.py
@@ -0,0 +1,46 @@
+from paramiko import SSHClient
+from scp import SCPClient
+import os
+
+ssh = SSHClient()
+
+host = '66.175.219.233'
+user = 'mgp214'
+key_filename = R'C:\Users\mgp214\.ssh\id_rsa'
+ssh.load_system_host_keys()
+
+print(f"Connecting to {host} as {user} using keyfile {key_filename}...")
+ssh.connect(host,
+            username=user,
+            key_filename=key_filename)
+
+# Define progress callback that prints the current percentage completed for the file
+
+
+def progress(filename, size, sent):
+    print("%s\'s progress: %.2f%%   \r" %
+          (filename, float(sent)/float(size)*100))
+
+
+# SCPCLient takes a paramiko transport as an argument
+scp = SCPClient(ssh.get_transport(), progress=progress)
+
+cwd = os.path.join(os.getcwd(), 'src')
+remote_dir = '/home/mgp214/plastic'
+
+print(f'Clearing {remote_dir}')
+ssh.exec_command(f'rm -rf {remote_dir}/src/*')
+
+print(f'Putting {cwd} on remote at {remote_dir}')
+scp.put(f'{cwd}', recursive=True,
+        remote_path=remote_dir)
+
+scp.close()
+print('done copying...')
+
+start_cmd = f'{remote_dir} npm i && setsid npm start'
+print(f'executing {start_cmd} on remote...')
+ssh.exec_command(start_cmd)
+
+ssh.close()
+print('done!')
