c7d0c27e02054e1868e93c1fc0c58c56816b68f3
Matthew Popp <mgp214@gmail.com>
2020-09-26 15:00:00 -0700

    template editor work
    - now with actual proper layout! jfc.

--- frontend/plastic/lib/widgets/template/edit_template_widget.dart
+++ frontend/plastic/lib/widgets/template/edit_template_widget.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:plastic/model/template.dart';
 import 'package:plastic/utility/style.dart';
-import 'package:plastic/widgets/components/border_button.dart';
 import 'package:plastic/widgets/components/splash_list_tile.dart';
 
 class EditTemplateWidget extends StatefulWidget {
@@ -17,6 +16,7 @@ class EditTemplateWidget extends StatefulWidget {
 class EditTemplateState extends State<EditTemplateWidget> {
   Map<String, TextEditingController> _metadataControllers;
   Map<String, FocusNode> _metadataNodes;
+  Map<String, Key> _metadataKeys;
   Map<TemplateField, TextEditingController> _fieldControllers;
   Map<TemplateField, FocusNode> _fieldNodes;
 
@@ -28,6 +28,7 @@ class EditTemplateState extends State<EditTemplateWidget> {
     _fieldControllers = Map();
     _fieldNodes = Map();
     _metadataNodes = Map();
+    _metadataKeys = Map();
     fieldKeys = Map();
     super.initState();
   }
@@ -78,8 +79,10 @@ class EditTemplateState extends State<EditTemplateWidget> {
     var newField = TemplateField(
         name: 'new ${TemplateField.getFriendlyName(fieldType)} field',
         type: fieldType);
-    if (widget.template.fields.firstWhere((f) => f.main, orElse: () => null) ==
-        null) newField.main = true;
+    if (fieldType == FieldType.STRING &&
+        widget.template.fields
+                .firstWhere((f) => f.main == true, orElse: () => null) ==
+            null) newField.main = true;
     widget.template.fields.add(newField);
     setState(() {});
     Navigator.pop(context);
@@ -90,6 +93,7 @@ class EditTemplateState extends State<EditTemplateWidget> {
       if (_fieldControllers[field] != null) return;
       var controller = TextEditingController(text: field.name);
       _fieldControllers[field] = controller;
+      fieldKeys[field] = UniqueKey();
 
       var node = FocusNode();
       _fieldNodes[field] = node;
@@ -209,50 +213,75 @@ class EditTemplateState extends State<EditTemplateWidget> {
         );
         break;
     }
-    Key key;
-    if (fieldKeys.containsKey(field)) {
-      key = fieldKeys[field];
-    } else {
-      key = UniqueKey();
-      fieldKeys[field] = key;
-    }
-    return Card(
-      key: key,
-      color: Style.inputField,
-      child: SplashListTile(
-        color: Style.accent,
-        onTap: () => Flushbar(
-          message: "Drag to rearrange fields",
-          duration: Style.snackDuration,
-        )..show(context),
-        child: Row(
-          children: [
-            Expanded(
-              child: cardContents,
-            ),
-            Icon(
-              Icons.reorder,
-              color: Style.accent,
-            ),
-          ],
+    return IntrinsicHeight(
+      key: fieldKeys[field],
+      child: Card(
+        color: Style.inputField,
+        child: SplashListTile(
+          color: Style.accent,
+          onTap: () => Flushbar(
+            message: "Drag to rearrange fields",
+            duration: Style.snackDuration,
+          )..show(context),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IntrinsicHeight(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Expanded(
+                      child: cardContents,
+                    ),
+                    VerticalDivider(
+                      width: 15,
+                      thickness: 1,
+                      color: Style.background,
+                      indent: 10,
+                      endIndent: 10,
+                    ),
+                    Icon(
+                      Icons.reorder,
+                      color: Style.background,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  List<Widget> _getChildren() {
-    var children = List<Widget>();
+  Widget _getReorderableFieldWidget() => Expanded(
+        child: ReorderableListView(
+          onReorder: (int oldIndex, int newIndex) {
+            var field = widget.template.fields[oldIndex];
+            widget.template.fields.removeAt(oldIndex);
+            widget.template.fields.insert(newIndex, field);
+            setState(() {});
+          },
+          children:
+              widget.template.fields.map((f) => _getFieldWidget(f)).toList(),
+        ),
+      );
 
+  Widget _getTemplateNameField() {
     FocusNode node;
     TextEditingController controller;
+    Key key;
     if (_metadataControllers.keys.contains('name')) {
       controller = _metadataControllers['name'];
       node = _metadataNodes['name'];
+      key = _metadataKeys['name'];
     } else {
       controller = TextEditingController(text: widget.template.name);
       node = FocusNode();
+      key = UniqueKey();
       _metadataControllers['name'] = controller;
       _metadataNodes['name'] = node;
+      _metadataKeys['name'] = key;
     }
     node.addListener(() {
       if (node.hasFocus) {
@@ -260,7 +289,8 @@ class EditTemplateState extends State<EditTemplateWidget> {
             TextSelection(baseOffset: 0, extentOffset: controller.text.length);
       }
     });
-    var templateNameField = TextField(
+    return TextField(
+      key: key,
       focusNode: node,
       controller: controller,
       decoration: InputDecoration(
@@ -272,93 +302,118 @@ class EditTemplateState extends State<EditTemplateWidget> {
         widget.template.name = value;
       }),
     );
-
-    children.add(templateNameField);
-    children.add(Divider(
-      color: Style.accent,
-    ));
-
-    var fieldChildren = List<Widget>();
-
-    for (var field in widget.template.fields) {
-      fieldChildren.add(_getFieldWidget(field));
-    }
-
-//TODO: replace with https://github.com/knopp/flutter_reorderable_list
-    var reorderableListWidget = Container(
-      height: MediaQuery.of(context).size.height - 350,
-      child: ReorderableListView(
-        onReorder: (int oldIndex, int newIndex) {
-          var field = widget.template.fields[oldIndex];
-          widget.template.fields.removeAt(oldIndex);
-          widget.template.fields.insert(newIndex, field);
-          setState(() {});
-        },
-        children: fieldChildren,
-      ),
-    );
-
-    if (fieldChildren.length > 0) children.add(reorderableListWidget);
-
-    children.add(BorderButton(
-      color: Style.accent,
-      content: "Add a new field",
-      onPressed: _onAddNewFieldPressed,
-    ));
-
-    children.add(BorderButton(
-      color: Style.primary,
-      content: "Save",
-      onPressed: () => null,
-    ));
-
-    children.add(BorderButton(
-      color: Style.error,
-      content: "Cancel",
-      onPressed: () {
-        showDialog(
-          context: context,
-          builder: (context) => SimpleDialog(
-            backgroundColor: Style.background,
-            title: Padding(
-              padding: EdgeInsets.only(bottom: 15),
-              child: Text(
-                "Discard new template?",
-                style: Style.getStyle(FontRole.Display3, Style.accent),
-              ),
-            ),
-            children: [
-              SimpleDialogOption(
-                child: Text(
-                  "Stay here",
-                  style: Style.getStyle(FontRole.Display3, Style.primary),
-                ),
-                onPressed: () => Navigator.pop(context),
-              ),
-              SimpleDialogOption(
-                child: Text(
-                  "Confirm cancel",
-                  style: Style.getStyle(FontRole.Display3, Style.error),
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    ));
-
-    return children;
   }
 
   @override
-  Widget build(BuildContext context) => Material(
-        color: Style.background,
-        child: ListView(
-          children: _getChildren(),
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Style.background,
+        floatingActionButton: FloatingActionButton(
+          shape: CircleBorder(side: BorderSide(color: Style.primary, width: 3)),
+          backgroundColor: Colors.transparent,
+          child: Icon(
+            Icons.menu,
+            color: Style.primary,
+          ),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => SimpleDialog(
+                backgroundColor: Style.background,
+                children: [
+                  SimpleDialogOption(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.add,
+                          color: Style.primary,
+                        ),
+                        Text("Add a new field",
+                            style: Style.getStyle(
+                                FontRole.Display3, Style.primary)),
+                      ],
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      _onAddNewFieldPressed();
+                    },
+                  ),
+                  SimpleDialogOption(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.save,
+                          color: Style.primary,
+                        ),
+                        Text("Save template",
+                            style: Style.getStyle(
+                                FontRole.Display3, Style.primary)),
+                      ],
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  SimpleDialogOption(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.cancel,
+                          color: Style.error,
+                        ),
+                        Text(
+                          "Discard",
+                          style: Style.getStyle(FontRole.Display3, Style.error),
+                        ),
+                      ],
+                    ),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => SimpleDialog(
+                          backgroundColor: Style.background,
+                          title: Padding(
+                            padding: EdgeInsets.only(bottom: 15),
+                            child: Text(
+                              "Discard new template?",
+                              style: Style.getStyle(
+                                  FontRole.Display3, Style.accent),
+                            ),
+                          ),
+                          children: [
+                            SimpleDialogOption(
+                              child: Text(
+                                "Stay here",
+                                style: Style.getStyle(
+                                    FontRole.Display3, Style.primary),
+                              ),
+                              onPressed: () => Navigator.pop(context),
+                            ),
+                            SimpleDialogOption(
+                              child: Text(
+                                "Confirm cancel",
+                                style: Style.getStyle(
+                                    FontRole.Display3, Style.error),
+                              ),
+                              onPressed: () {
+                                Navigator.pop(context);
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  )
+                ],
+              ),
+            );
+          },
+        ),
+        body: Column(
+          children: [
+            _getTemplateNameField(),
+            _getReorderableFieldWidget(),
+          ],
         ),
       );
 }
