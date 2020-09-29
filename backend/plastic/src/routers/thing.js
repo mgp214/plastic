92af0b0a3c18dac35f6c65b39de6bbc4c74ca2c1
Matthew Popp <mgp214@gmail.com>
2020-09-28 22:59:23 -0700

    first pass thing validation & fixes snacks
    closes #31
    snacks weren't showing because their duration was... 4 milliseconds. lol. corrected to 4000 (closes #113)

--- backend/plastic/src/routers/thing.js
+++ backend/plastic/src/routers/thing.js
@@ -1,14 +1,48 @@
 const express = require('express');
 const Thing = require('../models/Thing');
+const Template = require('../models/Template');
 const auth = require('../middleware/auth');
 const router = express.Router();
 
+// Make sure that the given thing is valid according to its template.
+async function validateAgainstTemplate(thing) {
+	const template = await Template.findById(thing.templateId);
+
+	// first check that each template field is present and valid in thing
+	for (var i = 0; i < template.fields.toBSON().length; i++) {
+		var templateField = template.fields.toBSON()[i];
+
+		var matchingCount = 0;
+		for (var thingField of thing.toBSON().fields) {
+			matchingCount += thingField._id.toString() == templateField._id.toString() ? 1 : 0;
+		}
+		// thing.fields.toBSON().reduce(
+		// 	(matchingCount, currentField) => matchingCount += (currentField._id == templateField._id ? 1 : 0));
+		if (matchingCount != 1) return false;
+		var matchingIndex = thing.fields.toBSON().findIndex(f => f._id.toString() == templateField._id.toString());
+		if (matchingIndex == -1) return false;
+		var matchingField = thing.fields.toBSON()[matchingIndex];
+		if (matchingField.name != templateField.name) return false;
+	}
+
+	// finally, make sure that there aren't any extra fields in the thing.
+	if (template.fields.toBSON().length != thing.fields.toBSON().length) return false;
+
+	// made it to the end, thing is valid.
+	return true;
+}
 
 // Create a new thing, or update if it exists already
 router.post('/things/save', auth, async (req, res) => {
 	try {
 		const thing = new Thing(req.body);
 		thing.userId = req.user._id;
+		console.log('validating thing [' + thing.id.toString() + '] against its template ' + thing.templateId.toString());
+		if (!await validateAgainstTemplate(thing)) {
+			res.status(400).statusMessage = 'thing does not match its template.';
+			res.send();
+			return;
+		}
 		console.log('saving thing ' + thing._id.toString());
 		await Thing.findOneAndUpdate(
 			{ _id: thing._id },
