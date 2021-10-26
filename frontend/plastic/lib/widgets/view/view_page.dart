7464a53326880c16df57f2bfe6e974682d54b30f
Matthew Popp <mgp214@gmail.com>
2021-10-25 19:10:40 -0700

    replaced RefreshIndicator with refresh button on Views
    closes #200

--- frontend/plastic/lib/widgets/view/view_page.dart
+++ frontend/plastic/lib/widgets/view/view_page.dart
@@ -18,9 +18,12 @@ class ViewPage extends StatefulWidget {
   State<StatefulWidget> createState() => ViewPageState();
 }
 
-class ViewPageState extends State<ViewPage> {
+class ViewPageState extends State<ViewPage> with TickerProviderStateMixin {
+  AnimationController _refreshAnimationController;
   @override
   void initState() {
+    _refreshAnimationController =
+        AnimationController(duration: Duration(milliseconds: 500), vsync: this);
     refresh();
     super.initState();
   }
@@ -53,23 +56,18 @@ class ViewPageState extends State<ViewPage> {
       backgroundColor: Motif.background,
       body: Stack(
         children: [
-          RefreshIndicator(
-              child: SingleChildScrollView(
-                physics: AlwaysScrollableScrollPhysics(),
-                child: Container(
-                  child: ViewFrameCard(
-                    frame: widget.view.root,
-                    rebuildLayout: (isDragging) => setState(() {}),
-                    resetLayout: (f) => setState(() {
-                      widget.view.root = f;
-                    }),
-                    isLocked: true,
-                    isEditing: false,
-                  ),
-                  height: MediaQuery.of(context).size.height,
-                ),
-              ),
-              onRefresh: () => Future(refresh)),
+          Container(
+            child: ViewFrameCard(
+              frame: widget.view.root,
+              rebuildLayout: (isDragging) => setState(() {}),
+              resetLayout: (f) => setState(() {
+                widget.view.root = f;
+              }),
+              isLocked: true,
+              isEditing: false,
+            ),
+            height: MediaQuery.of(context).size.height,
+          ),
           Positioned(
             bottom: 10 + MediaQuery.of(context).viewInsets.bottom,
             right: 10,
@@ -118,6 +116,46 @@ class ViewPageState extends State<ViewPage> {
                           );
                         });
                       });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 10,
+            right: 10,
+            child: Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(color: Motif.title, width: 3),
+                      bottom: BorderSide(color: Motif.title, width: 3),
+                      right: BorderSide(color: Motif.title, width: 3),
+                      left: BorderSide(color: Motif.title, width: 3),
+                    ),
+                    shape: BoxShape.circle,
+                    color: Motif.background,
+                  ),
+                  child: InkWell(
+                    child: Padding(
+                      padding: EdgeInsets.all(5),
+                      child: RotationTransition(
+                        turns: Tween(begin: 0.0, end: 1.0)
+                            .animate(_refreshAnimationController),
+                        child: Icon(
+                          Icons.refresh,
+                          color: Motif.title,
+                          size: Constants.iconSize,
+                        ),
+                      ),
+                    ),
+                    onTap: () {
+                      _refreshAnimationController.forward().then((value) {
+                        _refreshAnimationController.reset();
+                      });
+                      refresh();
                     },
                   ),
                 ),
